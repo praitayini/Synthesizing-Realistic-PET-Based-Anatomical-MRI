c350536dff53e4f99a04ad3ad54b52594223aafc
praitayini <31248960+praitayini@users.noreply.github.com>
2019-02-24 18:53:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 25 12:37:32 2018
+@author: Karl, PK, Tejas, Saurabh
+"""
+
 import tensorflow as tf
 import numpy as np
 import os
